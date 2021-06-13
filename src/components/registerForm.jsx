3985a7f685f4eae33db0cd3d0bac9a8b8f779502
Shehan Nandathilaka <shehann@codegen.net>
2021-06-13 14:54:52 +0530

    JWT implementation

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -26,7 +26,7 @@ class RegisterForm extends Form {
     try {
       const response = await userService.registerUser(this.state.data);
       localStorage.setItem("token", response.headers["x-auth-token"]);
-      this.props.history.push("/");
+      window.location = "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
